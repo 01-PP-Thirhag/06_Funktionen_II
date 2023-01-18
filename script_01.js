70847d8836351345374473b1ce4fa11daaf850ab
Thirhag <meigraf@hotmail.de>
2023-01-18 10:58:25 +0100

    Multiply

--- script_01.js
+++ script_01.js
@@ -11,27 +11,32 @@
 4. Ausgabe in Konsole : check!
 */
 
+// module: multiply a * b |  test:
+output(multiply(2,3));
+output(multiply(2,-9));
+output(multiply(-1,-3));
+function multiply (a,b) {
+	return a * b
 
 
-// module: addition a + b |  test:
-output(add(2,2));
-output(add(2,-2));
-output(add(2,0));
-function add(a,b) {
-	return a + b;
+// module: subtraction a - b |  test:
+// output(subtract(2,3));
+// output(subtract(2,-9));
+// output(subtract(-1,-3));
+// function subtract (a,b) {
+// 	return a - b
 }
 
 
 // module:  addition a + b | test:
-output(add(2,3));
-output(add(2,-9));
-output(add(-1,-3));
+// output(add(2,3));
+// output(add(2,-9));
+// output(add(-1,-3));
 
-function add(a,b) {
-	let c = a + b;
-	return c; 
-	
-}
+// function add(a,b) {
+// 	let c = a + b;
+// 	return c; 	
+// }
 // module: output | test:
 // output("hello");
 // output(2);
