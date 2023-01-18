6750e38efef20983b025ceee4f23651e599f73db
Thirhag <meigraf@hotmail.de>
2023-01-18 12:57:51 +0100

    Iteration2

--- script_01.js
+++ script_01.js
@@ -6,7 +6,7 @@
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
 1. Dateneingabe + -überprüfung :
-2. Auswahl Rechenart :
+2. Auswahl Rechenart : check!
 3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
@@ -25,28 +25,22 @@ output(calculator(3,2,":"));
 output(calculator(3,2,"/"));
 output(calculator(3,0,"/"));
 output(calculator(3,2,"#?!"));
-
 function calculator(a,b,op) {
 	switch (op) {
 		case "+": // addition
-			return "Ergebnis addition";
+			return add(a,b);
 		case "-": // subtraktion
-			return "Ergebnis subtraktion";
+			return subtract(a,b);
 		case "*": // multiplikation
-			return "Ergebnis multiplikation";
+			return multiply(a,b);
 		case ":": // divison
 		case "/": // divison
-			return "Ergebnis divison";
+			return divide(a,b);
 		default:
 			return ERROR_STR_GEN;
 	}
 }
-
 // module: division a / b |  test:
-// output(divide(4,2));
-// output(divide(3,2));
-// output(divide(3,-2));
-// output(divide(3,0));
 function divide(a,b) {
 	if (b == 0) { 
 		return ERROR_STR_DIV;
@@ -55,33 +49,22 @@ function divide(a,b) {
 }
 
 // module: multiplication a * b |  test:
-// output(multiply(3,2));
-// output(multiply(3,-2));
-// output(multiply(3,0));
 function multiply(a,b) {
     return a * b; 
 }
 
 // module: subtraction a - b |  test:
-// output(subtract(3,2));
-// output(subtract(3,-2));
-// output(subtract(3,0));
 function subtract(a,b) {
 	return a - b;
 }
 
 // module: addition a + b |  test:
-// output(add(2,2));
-// output(add(2,-2));
-// output(add(2,0));
 function add(a,b) {
 	let c = a + b;
 	return c; 	
 }
 
 // module: output | test:
-// output("hello");
-// output(2);
 function output(outputData) {
 	console.log(outputData);
 }
