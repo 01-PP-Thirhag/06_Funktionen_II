606255f9b9dc98ada157f6fe05aba738c84c2255
Thirhag <meigraf@hotmail.de>
2023-01-18 12:28:25 +0100

    module: calculator

--- script_01.js
+++ script_01.js
@@ -13,37 +13,72 @@
 
 // Konstanten
 const ERROR_STR_DIV = "Division durch 0 nicht möglich" 
+const ERROR_STR_GEN = "irgendetwas ging schief!"
+
+// module: calculator | test:
+// agreement: "+","-","*",":"
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"&%"));
+
+function calculator(a,b,op) {
+	
+	switch (op) {
+		case "+": // addiion
+			return "Ergebnis addition"
+		case "-": // subtraction
+			return "Ergebnis subtraction"
+		case "*": // multiplikation
+			return "Ergebnis multiplikation"
+		case ":": // division
+		case "/": // division
+			return "Ergebnis division"
+		default:
+			return ERROR_STR_GEN
+	}
+	
+	}
 
 // modul division a / b  | test:
-output(divide(10,2));
-output(divide(20,10));
-output(divide(100,0));
-function divide(a,b) {
+// output(divide(10,2));
+// output(divide(20,10));
+// output(divide(100,0));
+// function divide(a,b) {
 
-	if (b==0) {
-		return ERROR_STR_DIV;
-	} else {
-		return a/b
-	}
-}
+// 	if (b==0) {
+// 		return ERROR_STR_DIV;
+// 	} else {
+// 		return a/b
+// 	}
+// }
+//Konstante Var 2
+// if (b==0) {
+// 	return ERROR_STR_DIV;
+// 	return a/b
+// }
 
 
 // module: multiply a * b |  test:
 // output(multiply(2,3));
 // output(multiply(2,-9));
 // output(multiply(-1,-3));
-// function multiply (a,b) {
+function multiply (a,b) {
 // 	return a * b
-//}
+}
 
 
 // module: subtraction a - b |  test:
 // output(subtract(2,3));
 // output(subtract(2,-9));
 // output(subtract(-1,-3));
-// function subtract (a,b) {
-// 	return a - b
-// }
+function subtract (a,b) {
+
+	return a - b
+}
 
 
 // module:  addition a + b | test:
@@ -51,10 +86,10 @@ function divide(a,b) {
 // output(add(2,-9));
 // output(add(-1,-3));
 
-// function add(a,b) {
-// 	let c = a + b;
-// 	return c; 	
-// }
+function add(a,b) {
+	let c = a + b;
+	return c; 	
+}
 
 // module: output | test:
 // output("hello");
